22819a1cfc578e7455e29fdabe62e54d81d673ae
Sittisukintaruk <fah78963214@gmail.com>
2022-07-05 00:17:44 +0700

    add

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+    }
+    
+  },
+  plugins: [],
+}
+
